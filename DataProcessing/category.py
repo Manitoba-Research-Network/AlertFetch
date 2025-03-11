8b1f4e2078fa29b8d567bdcf7969957b6176d5e0
VanJackal <v@njackal.com>
2025-03-11 09:30:00 -0500

    fixed the query issue

--- DataProcessing/category.py
+++ DataProcessing/category.py
@@ -10,9 +10,9 @@ client = Elasticsearch(
     api_key= os.getenv("API_KEY")
 )
 
-res= client.search(index="logs-omm_one", q='process where host.os.type == "windows" and event.type == "start" and process.parent.name : "Zoom.exe" and process.name : ("cmd.exe", "powershell.exe", "pwsh.exe", "powershell_ise.exe")')
+res= client.eql.search(index="logs-omm_one", query='process where host.os.type == "windows" and event.type == "start" and process.parent.name : "Zoom.exe" and process.name : ("cmd.exe", "powershell.exe", "pwsh.exe", "powershell_ise.exe")')
 
-print(len(res.raw["hits"]["hits"]))
+print(res)
 
 
 
