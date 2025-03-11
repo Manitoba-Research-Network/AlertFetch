13a8569dca49d4b96bf1a19e82012e329a170935
VanJackal <v@njackal.com>
2025-03-11 10:32:15 -0500

    get query from toml

--- lib/retrieval.py
+++ lib/retrieval.py
@@ -1,5 +1,5 @@
 from elasticsearch import Elasticsearch
-
+import toml
 
 def get_ids(client:Elasticsearch,index:str, query:str)-> set:
     res = client.eql.search(index=index, query=query, fields=[])
@@ -9,3 +9,11 @@ def get_ids(client:Elasticsearch,index:str, query:str)-> set:
         output.add(hit["_id"])
     return output
 
+def toml_read_query(path:str):
+    with open(path) as f:
+        rule = toml.loads(f.read())
+
+    if "query" not in rule["rule"]:
+        return None
+
+    return rule["rule"]["query"]
