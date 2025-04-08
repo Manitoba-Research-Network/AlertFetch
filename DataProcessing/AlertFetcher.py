a43a2584dbe9f0e343f14ba67b67d98e0945f545
jackal <v@njackal.com>
2025-04-08 11:22:42 -0500

    output to correct format

--- DataProcessing/AlertFetcher.py
+++ DataProcessing/AlertFetcher.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import sys
 import datetime
 
+from lib.output import write_jsonl
 from lib.processing import clean_entry, clean_entries
 from lib.retrieval import get_alert_ids, get_from_ids, get_inverse_from_ids
 
@@ -11,7 +12,7 @@ DEFAULT_INDEX_PAT = ".internal.alerts-security.alerts-default-*"
 DEFAULT_START_DATE = (datetime.datetime.now(datetime.UTC) - datetime.timedelta(hours=5)).isoformat()
 DEFAULT_END_DATE = datetime.datetime.now(datetime.UTC).isoformat()
 
-def main(es_url, api_key, index, start, end):
+def main(es_url, api_key, index, start, end, out):
     client = Elasticsearch(
         hosts=[es_url],
         api_key= api_key
@@ -26,9 +27,7 @@ def main(es_url, api_key, index, start, end):
     eventsPass = get_inverse_from_ids(client, ids)
     cleanedPass = clean_entries(eventsPass)
 
-
-    print(cleaned)
-    print(cleanedPass)
+    write_jsonl("out.jsonl", cleaned, cleanedPass)
 
 
 if __name__ == "__main__":
@@ -36,6 +35,10 @@ if __name__ == "__main__":
     start = kwargs['start'] if "start" in kwargs else DEFAULT_START_DATE
     end = kwargs['end'] if 'end' in kwargs else DEFAULT_END_DATE
     index = kwargs['index'] if 'index' in kwargs else DEFAULT_INDEX_PAT
+    if "out" not in kwargs:
+        print("'out' is a required keyword parameter")
+        exit(1)
+    out = kwargs['out']
 
     load_dotenv()
-    main(os.getenv("ES_URL"), os.getenv("API_KEY"), index, start, end)
+    main(os.getenv("ES_URL"), os.getenv("API_KEY"), index, start, end, out)
--- lib/output.py
+++ lib/output.py
@@ -9,3 +9,10 @@ def write_json(path, data):
     with open(path, "w") as f:
         out = json.dumps(data,sort_keys=True, indent=4)
         f.write(out)
+
+def write_jsonl(path,alert, no_alert):
+    with open(path, "w") as f:
+        for e in alert:
+            f.write(json.dumps({"text":e, "label":1}) + "\n")
+        for e in no_alert:
+            f.write(json.dumps({"text": e, "label": 0}) + "\n")
