f1716b3d8ba43b0e2e9aca23051ef883a4a5d734
VanJackal <v@njackal.com>
2025-06-13 09:23:22 -0500

    moved pipelines to seperate library file

--- RunAI.py
+++ RunAI.py
@@ -1,6 +1,7 @@
 import json
 
 from ai.AiClient import AIClient
+from pipeline import *
 from pipeline.PipelineRunner import PipelineRunner
 from pipeline.PipelineLoopStep import PipelineLoopStep
 from pipeline.steps import *
@@ -14,35 +15,14 @@ with open("openai.json") as f:
 if __name__ == "__main__":
     client = AIClient(**config, model=MODEL)
 
-    pipeline = PipelineRunner("SummarizeEvent")
-    pipeline.add_step(LambdaPipelineStep("Get First List Value & Parse to json",lambda x: json.loads(x["text"]))) # get first event
-    pipeline.add_step(AIJsonPreprocess()) # get the AI input ready
-    pipeline.add_step(PromptAddStep("write a 4 sentence summary of this alert data")) # add prompt to data
-    pipeline.add_step(PrintData("Print Prompt"))
-    pipeline.add_step(AIRunStep(client, "you are a security expert")) # summarize the event
+    multipipe = MultiEventSummary(client)
 
-    multipipe = PipelineRunner("MultiEventRunner")
-    multipipe.add_step(ReadJsonlFileStep())
-    multipipe.add_step(PipelineLoopStep(pipeline, "SingleSummary"))
-    multipipe.add_step(AggregateResponsesStep("Summary #"))
-    multipipe.add_step(PromptAddStep("The following are summaries from alerts that are all related to each other, write a brief summary of these summaries"))
-    multipipe.add_step(PrintData("Aggregate Summary Prompt"))
-    multipipe.add_step(AIRunStep(client, "you are a security expert"))
+    print(multipipe.execute("./out/BU_small.jsonl"))
 
-    #print(multipipe.execute("./out/MRNet100.jsonl"))
-
-    json_format_pipe = PipelineRunner("JsonFormat")
-    json_format_pipe.add_step(LambdaPipelineStep("parse json",lambda x: json.loads(x["text"]))) # get first event
-    json_format_pipe.add_step(AIJsonPreprocess()) # get the AI input ready
-
-    mega_pipe = PipelineRunner("MultiEvent1Summary")
-    mega_pipe.add_step(ReadJsonlFileStep())
-    mega_pipe.add_step(PipelineLoopStep(json_format_pipe, "format json"))
-    mega_pipe.add_step(AggregateResponsesStep("Event #"))
-    mega_pipe.add_step(PromptAddStep("The following are related events, please write a brief summary of these events"))
-    mega_pipe.add_step(PrintData("Event Summary Prompt"))
-    mega_pipe.add_step(AIRunStep(client, "you are a security expert"))
-
-    print(mega_pipe.execute("./out/BU_small.jsonl"))
+    mega_pipe = MultiEventSingleSummary(
+        client,
+        "The following are related events, please write a 1 paragraph summary of these events"
+    )
+    #print(mega_pipe.execute("./out/BU_small.jsonl"))
 
 
---
+++ pipeline/Pipelines.py
@@ -0,0 +1,50 @@
+# This file contains various prefab pipelines, mostly for ai execution
+from pipeline.PipelineRunner import PipelineRunner
+from pipeline.PipelineLoopStep import PipelineLoopStep
+from pipeline.steps import *
+
+
+def MultiEventSummary(client:AIClient,
+                      prompt="The following are summaries from alerts that are all related to each other, write a brief summary of these summaries"
+                      ) -> PipelineRunner:
+    """
+    summarize multiple events using intermediate summaries for each event
+    """
+
+    pipeline = PipelineRunner("SummarizeEvent")
+    pipeline.add_step(LambdaPipelineStep("Get First List Value & Parse to json", lambda x: json.loads(x["text"])))  # get first event
+    pipeline.add_step(AIJsonPreprocess())  # get the AI input ready
+    pipeline.add_step(PromptAddStep("write a 4 sentence summary of this alert data"))  # add prompt to data
+    pipeline.add_step(PrintData("Print Prompt"))
+    pipeline.add_step(AIRunStep(client, "you are a security expert"))  # summarize the event
+
+    multipipe = PipelineRunner("MultiEventRunner")
+    multipipe.add_step(ReadJsonlFileStep())
+    multipipe.add_step(PipelineLoopStep(pipeline, "SingleSummary"))
+    multipipe.add_step(AggregateResponsesStep("Summary #"))
+    multipipe.add_step(PromptAddStep(prompt))
+    multipipe.add_step(PrintData("Aggregate Summary Prompt"))
+    multipipe.add_step(AIRunStep(client, "you are a security expert"))
+
+    return multipipe
+
+def MultiEventSingleSummary(
+        client:AIClient,
+        prompt: str = "The following are related events, please write a brief summary of these events"
+) -> PipelineRunner:
+    """
+    summarize all events in single ai prompt
+    """
+    json_format_pipe = PipelineRunner("JsonFormat")
+    json_format_pipe.add_step(LambdaPipelineStep("parse json",lambda x: json.loads(x["text"]))) # get first event
+    json_format_pipe.add_step(AIJsonPreprocess()) # get the AI input ready
+
+    mega_pipe = PipelineRunner("MultiEvent1Summary")
+    mega_pipe.add_step(ReadJsonlFileStep())
+    mega_pipe.add_step(PipelineLoopStep(json_format_pipe, "format json"))
+    mega_pipe.add_step(AggregateResponsesStep("Event #"))
+    mega_pipe.add_step(PromptAddStep(prompt))
+    mega_pipe.add_step(PrintData("Event Summary Prompt"))
+    mega_pipe.add_step(AIRunStep(client, "you are a security expert"))
+
+    return mega_pipe
